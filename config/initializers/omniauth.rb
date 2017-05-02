82fe61ba329ab41911cdd7755e838a99f6681794
ojamal02 <ojamal02@gmail.com>
2017-05-02 16:29:45 -0400

    fixed facebook authentication

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, 'my facebook app id', 'my facebook app secret', {:client_options => {:ssl => {:ca_file => Rails.root.join("cacert.pem").to_s}}}
+  provider :facebook, '1669503223357514', 'cf2d9576c849a3cfaea7237982c719e3' , {:client_options => {:ssl => {:ca_file => Rails.root.join("cacert.pem").to_s}}}
 end
