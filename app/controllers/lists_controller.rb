c0b09cdc2bc0b6d7f9ae0b353e737736de28a537
ojamal02 <ojamal02@gmail.com>
2017-05-04 17:30:28 -0400

    made suggestion tables

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,63 @@
+class ListsController < ApplicationController
+
+  def index
+    @lists = List.all.order("created_at DESC")
+  end
+
+  def show
+    @list = List.find(params[:id])
+  end
+
+  def new
+    if current_user.present?
+      @list = List.new
+    else
+      flash[:error] = "Please log in before trying to submit a list!"
+      redirect_to root_path
+    end
+  end
+
+  def create
+    @list = List.new(list_params)
+    @list.user= current_user
+    if @list.save
+      flash[:notice] = "list submitted successfully!"
+      redirect_to list_path(@list)
+    else
+      @errors = @list.errors.full_messages
+      render :new
+    end
+  end
+
+  def edit
+    @list = List.find(params[:id])
+  end
+
+  def update
+    @list = List.find(params[:id])
+    @list.update_attributes(list_params)
+    if @list.save
+      flash[:success] = 'list successfully edited!'
+      redirect_to list_path(@list)
+    else
+      @errors = @list.errors.full_messages
+      render :new
+    end
+  end
+
+  def destroy
+      @list = List.find(params[:id])
+      @list.destroy
+      flash[:success] = "list deleted"
+      redirect_to root_path
+      end
+  end
+
+  private
+
+  def list_params
+    params.require(:list).permit(
+      :title,
+      :body
+    )
+  end
---
+++ app/models/list.rb
@@ -0,0 +1,6 @@
+class List < ApplicationRecord
+  validates :title, presence: true
+  validates :body, presence: true
+
+  belongs_to :user
+end
